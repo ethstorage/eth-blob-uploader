4d90b67c185b7d7f9cb15244d89324bd0b4ed5da
iteye <iteyelmp@gmail.com>
2024-02-02 11:56:07 +0800

    remove command

--- cli.js
+++ cli.js
@@ -5,29 +5,23 @@ program.version(require('./package.json').version);
 const {uploadToAddress} = require("./index");
 
 program
-    .option('-r, --rpc <rpc>', 'provider url')
-    .option('-p, --privateKey <privateKey>', 'private key')
-    .option('-f, --file <file>', 'upload file path')
-    .option('-t, --to <to>', 'to address')
+    .requiredOption('-r, --rpc <rpc>', 'provider url')
+    .requiredOption('-p, --privateKey <privateKey>', 'private key')
+    .requiredOption('-f, --file <file>', 'upload file path')
+    .requiredOption('-t, --to <to>', 'to address')
     .option('-d, --data [data]', 'call data')
     .option('-v, --value [value]', 'The value sent in each transaction. \n' +
         'If there are multiple transactions for a file, the total amount is "value*txCount".')
     .option('-c, --count [count]', 'send blob count, max is 6')
     .option('-n, --nonce [nonce]', 'transaction nonce')
     .option('-g, --gasPrice [gasPrice]', 'transaction gas price')
-    .option('-b, --blobPrice [blobPrice]', 'blob gas price');
+    .option('-b, --blobPrice [blobPrice]', 'blob gas price')
+    .parse(process.argv);
 
-program
-    .command('upload')
-    .description('upload files using eip-4884 blobs')
-    .action(() => {
-        const opts = program.opts();
-        uploadToAddress(
-            opts.rpc, opts.privateKey,
-            opts.file, opts.to,
-            opts.data, opts.value, opts.count,
-            opts.nonce, opts.gasPrice, opts.blobPrice
-        );
-    });
-
-program.parse(process.argv);
+const opts = program.opts();
+uploadToAddress(
+    opts.rpc, opts.privateKey,
+    opts.file, opts.to,
+    opts.data, opts.value, opts.count,
+    opts.nonce, opts.gasPrice, opts.blobPrice
+);
